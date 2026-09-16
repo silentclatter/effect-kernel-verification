eb852211af699a12523f1965a1859373fc81c1ac
silentclatter <silent.clatter@gmail.com>
2026-09-15 22:40:29 -0400

    Phase C-R2 T1: add constitutional dependency audit

--- effect_kernel_r0_2c_phase_c/EffectKernel/AxiomAudit.lean
+++ effect_kernel_r0_2c_phase_c/EffectKernel/AxiomAudit.lean
@@ -1,4 +1,5 @@
-import EffectKernel.T9_Lifecycle
+import EffectKernel.T1_Constitution
 
 #print axioms EffectKernel.T0_wellFormed
 #print axioms EffectKernel.T9_lifecycle
+#print axioms EffectKernel.T1_constitution
