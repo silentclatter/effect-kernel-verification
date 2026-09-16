717dedd9d3ad416605a0c9e7ccffa096d132050e
silentclatter <silent.clatter@gmail.com>
2026-09-15 22:49:25 -0400

    Phase C-R3 T2: add authority dependency audit

--- effect_kernel_r0_2c_phase_c/EffectKernel/AxiomAudit.lean
+++ effect_kernel_r0_2c_phase_c/EffectKernel/AxiomAudit.lean
@@ -1,5 +1,6 @@
-import EffectKernel.T1_Constitution
+import EffectKernel.T2_AuthorityAttenuation
 
 #print axioms EffectKernel.T0_wellFormed
 #print axioms EffectKernel.T9_lifecycle
 #print axioms EffectKernel.T1_constitution
+#print axioms EffectKernel.T2_transitiveAuthorityAttenuation
