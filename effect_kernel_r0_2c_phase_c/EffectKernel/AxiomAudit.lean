61e87cf31cb738269c84b1123969ec94075600c8
silentclatter <silent.clatter@gmail.com>
2026-09-15 23:02:20 -0400

    Phase C-R3 T4: add conservation dependency audit

--- effect_kernel_r0_2c_phase_c/EffectKernel/AxiomAudit.lean
+++ effect_kernel_r0_2c_phase_c/EffectKernel/AxiomAudit.lean
@@ -1,6 +1,8 @@
-import EffectKernel.T2_AuthorityAttenuation
+import EffectKernel.T4_BudgetConservation
 
 #print axioms EffectKernel.T0_wellFormed
 #print axioms EffectKernel.T9_lifecycle
 #print axioms EffectKernel.T1_constitution
 #print axioms EffectKernel.T2_transitiveAuthorityAttenuation
+#print axioms EffectKernel.T4_budgetConservation
+#print axioms EffectKernel.T4_e3_successor_epoch_start
